8dfd333c152dafa4b0ee4f62885bbd0cd5a84f0c
andygarrad <andygarrad@gmail.com>
2016-05-08 15:01:09 +0100

    added in styled alerts for win or lose, corrected the answer shown in alerts. added in microphone images too

--- getImage.php
+++ getImage.php
@@ -5,3 +5,16 @@
  * Date: 07-May-16
  * Time: 9:57 PM
  */
+
+$str = file_get_contents('images.json');
+$json = json_decode($str, true);
+
+//echo count($json['images']);
+$num=rand(0,count($json['images'])-1);
+
+$aReturn['count'] = $num;
+$aReturn['title'] = $json['images'][$num]['title'];
+$aReturn['source'] = $json['images'][$num]['source'];
+
+echo json_encode($aReturn);
+?>
